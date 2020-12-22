1d1ee23444c67bf714b559fabe626a8f7fa06831
Sneha Inguva <snehainguva@Snehas-MBP-2.attlocal.net>
2020-12-22 17:18:07 -0500

    attempting to call route adding and ifconfig commands from code

--- cmd/tuntap/main.go
+++ cmd/tuntap/main.go
@@ -2,10 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os/exec"
+	"time"
 
 	water "github.com/netvfy/tuntap"
 )
+
+type IPConfig struct {
+	IP      string
+	NetMask string
+	Gateway string
+	Subnet  string
+}
+
+// TODO(sneha): function will run in here
+func setupAddresses(iface string, ipConfig *IPConfig) error {
+	return nil
+}
 
 func main() {
 	// TODO(sneha):  modify tuntap package to have a function that checks for the existence of the interface
@@ -15,6 +30,7 @@ func main() {
 	config.Name = "utun9"
 	// Note: This is how the tun interface can be persisted
 	//config.Persist = true
+	// TODO(sneha): change permissions of the tun/tap interface to be in the same usergroup as this process
 
 	ifce, err := water.New(config)
 	if err != nil {
@@ -32,12 +48,58 @@ func main() {
 	// Despite the requirement of point-to-point, adding the above src, dst, and route
 	// let's us still see the traffic
 	// https://github.com/netvfy/tapcfg/blob/master/src/lib/tapcfg_unix_linux.h#L76
+	subnet := "10.100.0.0/16"
+	gateway := "10.100.0.1"
+	netmask := "255.255.0.0"
+	//addr := "10.100.0.3"
+
+	// STEP 2: Configure tun interface address
+	cmd2 := exec.Command("sudo", "ifconfig", "utun9", gateway, gateway, "netmask", netmask)
+	stderr, err := cmd2.StderrPipe()
+
+	log.Printf("Running ifconfig command and waiting for it to finish...")
+	err = cmd2.Start()
+	if err != nil {
+		log.Fatalf("Command failed to start with error: %v", err)
+	}
+
+	slurp, _ := ioutil.ReadAll(stderr)
+	fmt.Printf("%s\n", slurp)
+
+	if err := cmd2.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Command ifconfig successfully run...")
+
+	time.Sleep(2)
+
+	// TODO(sneha): create command wrapper
+	// STEP 1: Add Route
+	cmd := exec.Command("sudo", "route", "add", "-net", subnet, gateway, "-ifscope", "utun9")
+	stderr, err = cmd.StderrPipe()
+
+	log.Printf("Running add route command and waiting for it to finish...")
+	err = cmd.Start()
+	if err != nil {
+		log.Fatalf("Command failed to start with error: %v", err)
+	}
+
+	slurp, _ = ioutil.ReadAll(stderr)
+	fmt.Printf("%s\n", slurp)
+
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Command add route successfully run...")
+
 	for {
+		log.Println("starting the tun/tap listener...")
 		buff := make([]byte, 1500)
 		// read from the connection
 		n, err := ifce.Read(buff)
 		// TODO(sneha): switch statement to account for different errors
 		if err != nil {
+			log.Println("tun/tap listened stopped ")
 			log.Fatal(err)
 		}
 
