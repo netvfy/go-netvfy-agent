0445fd5ffa225016a5dc3dc11c55b7294ae6adc2
Sneha Inguva <snehainguva@Snehas-MacBook-Pro-2.local>
2021-01-03 21:16:50 -0500

    parametrized and cleaned up configuration

--- cmd/tuntap/main.go
+++ cmd/tuntap/main.go
@@ -1,60 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os/exec"
+	"runtime"
+	"strconv"
+	"strings"
 	"time"
 
 	water "github.com/netvfy/tuntap"
 )
 
+// IPConfig contains routing and interface configuration information
 type IPConfig struct {
-	IP      string
-	NetMask string
-	Gateway string
+	Netmask string
+	Addr    string
 	Subnet  string
 }
 
-// TODO(sneha): function will run in here
-func setupAddresses(iface string, ipConfig *IPConfig) error {
-	return nil
-}
-
-func main() {
-	// TODO(sneha):  modify tuntap package to have a function that checks for the existence of the interface
-	config := water.Config{
-		DeviceType: water.TUN,
-	}
-	config.Name = "utun9"
-	// Note: This is how the tun interface can be persisted
-	//config.Persist = true
-	// TODO(sneha): change permissions of the tun/tap interface to be in the same usergroup as this process
-
-	ifce, err := water.New(config)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// TODO(sneha): Assign IP address to interface
-	// Parametrize and make os.exec calls to this
-	// For now - doing this manually->
-	//        1. sudo ifconfig utun9 10.100.0.3 10.100.0.1 netmask 255.255.0.0
-	//	     Explanation: src is IP and dst is the gateway, this is a /16 subnet
-	//        2. sudo route add -net 10.100.0.0/16 10.100.0.1
-	//	     Explanation: route add subnet to gateway
-	//        3. ping 10.100.0.42
-	// Despite the requirement of point-to-point, adding the above src, dst, and route
-	// let's us still see the traffic
-	// https://github.com/netvfy/tapcfg/blob/master/src/lib/tapcfg_unix_linux.h#L76
-	subnet := "10.100.0.0/16"
-	gateway := "10.100.0.1"
-	netmask := "255.255.0.0"
-	//addr := "10.100.0.3"
+// configureInterface execs two commands
+func configureInterface(osName, iface string, ipConfig *IPConfig) error {
+	// TODO(sneha): consider how to do this for other operating systems
+	// var routeCmd *exec.Cmd
+	// var ipCmd *exec.Cmd
+	// switch osName {
+	// default:
+	// }
 
 	// STEP 2: Configure tun interface address
-	cmd2 := exec.Command("sudo", "ifconfig", "utun9", gateway, gateway, "netmask", netmask)
+	cmd2 := exec.Command("sudo", "ifconfig", "utun9", ipConfig.Addr, ipConfig.Addr, "netmask", ipConfig.Netmask)
 	stderr, err := cmd2.StderrPipe()
 
 	log.Printf("Running ifconfig command and waiting for it to finish...")
@@ -75,7 +52,7 @@ func main() {
 
 	// TODO(sneha): create command wrapper struct/funcs
 	// STEP 1: Add Route
-	cmd := exec.Command("sudo", "route", "add", "-net", subnet, gateway, "-ifscope", "utun9")
+	cmd := exec.Command("sudo", "route", "add", "-net", ipConfig.Subnet, ipConfig.Addr, "-ifscope", iface)
 	stderr, err = cmd.StderrPipe()
 
 	log.Printf("Running add route command and waiting for it to finish...")
@@ -92,8 +69,104 @@ func main() {
 	}
 	log.Println("Command add route successfully run...")
 
+	return nil
+}
+
+// validateInterfaceMacOs ensures that the interface should be of the format utun<Num>
+func validateInterface(osName, iface string) bool {
+	switch osName {
+	// OSX expects interface names utun
+	case "darwin":
+		if !strings.Contains(iface, "utun") {
+			return false
+		}
+
+		res := strings.Split(iface, "utun")
+		if len(res) != 2 {
+			return false
+		}
+
+		num, err := strconv.ParseInt(res[1], 10, 64)
+		if err != nil {
+			return false
+		}
+
+		if num < 0 {
+			return false
+		}
+	case "linux":
+		if !strings.Contains(iface, "tun") {
+			return false
+		}
+
+		res := strings.Split(iface, "tun")
+		if len(res) != 2 {
+			return false
+		}
+
+		num, err := strconv.ParseInt(res[1], 10, 64)
+		if err != nil {
+			return false
+		}
+
+		if num < 0 {
+			return false
+		}
+	default:
+		return false
+	}
+
+	return true
+}
+
+var interfaceFlag string
+
+func main() {
+	// Retrieve operating system name
+	osName := runtime.GOOS
+
+	flag.StringVar(&interfaceFlag, "interface", "utun9", "tun interface name for service")
+	flag.Parse()
+
+	// Validate name of interface
+	if !validateInterface(osName, interfaceFlag) {
+		log.Fatalf("invalid interface name: %v", interfaceFlag)
+	}
+
+	// TODO(sneha):  modify tuntap package to have a function that checks for the existence of the interface
+	config := water.Config{
+		DeviceType: water.TUN,
+	}
+	config.Name = interfaceFlag
+	// Note: This is how the tun interface can be persisted
+	//config.Persist = true
+	// TODO(sneha): change permissions of the tun/tap interface to be in the same usergroup as this process
+
+	ifce, err := water.New(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Add address to interface and routes
+	subnet := "198.18.0.0/16"
+	addr := "198.18.0.1"
+	netmask := "255.255.0.0"
+	exampleDst := "198.18.0.5"
+
+	ipConfig := &IPConfig{
+		Addr:    addr,
+		Netmask: netmask,
+		Subnet:  subnet,
+	}
+
+	err = configureInterface(osName, interfaceFlag, ipConfig)
+	if err != nil {
+		log.Fatalf("unable to configure agent: %v", err)
+	}
+
 	for {
 		log.Println("starting the tun/tap listener...")
+		log.Printf("Note traffic must be sourced from IP of iface like so: ping -S %v %v", addr, exampleDst)
 		buff := make([]byte, 1500)
 		// read from the connection
 		n, err := ifce.Read(buff)
