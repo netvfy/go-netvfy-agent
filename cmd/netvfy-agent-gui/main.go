503a1b575204b1a5940cf45684a9615dc3530334
Nicolas Bouliane <nbouliane@digitalocean.com>
2021-06-21 20:19:22 -0400

    revert this for now to keep the code working

--- cmd/netvfy-agent-gui/main.go
+++ cmd/netvfy-agent-gui/main.go
@@ -74,8 +74,9 @@ func InputBox(title, message, defaultAnswer string) (string, bool) {
 	out, err := exec.Command(
 		"osascript",
 		"-e",
-		fmt.Sprintf(`set T to text returned of (display dialog %s buttons {"Cancel", "OK"} default button "OK" with title %s default answer %s)`,
-			message, title, defaultAnswer)).Output()
+		`set T to text returned of (display dialog "`+
+			message+`" buttons {"Cancel", "OK"} default button "OK" with title "`+title+`" default answer "`+
+			defaultAnswer+`")`).Output()
 	if err != nil {
 		return "", false
 	}
